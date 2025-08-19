928e94d777777ddaf7fec231a484e3664ea75a6e
daniel <danielaraux@hotmail.com>
2025-08-19 10:41:16 +0200

    test

---
+++ docker-celio-main/src/product.php/clothes.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.2
+ */
+
+/**
+ * Database `celio`
+ */
+
+/* `celio`.`vet` */
+$clothes = array(
+  array('id' => '1','name' => 'T-shirt bleu','size' => 'M','price' => '19.99','type' => 't-shirt','gender' => 'homme','gallery' => 'tshirt_bleu_2.jpg,tshirt_bleu_3.jpg,tshirt_bleu_1.jpg','main' => 'tshirt_bleu_1.jpg'),
+  array('id' => '2','name' => 'Pantalon noir','size' => 'L','price' => '39.99','type' => 'pantalon','gender' => 'femme','gallery' => 'pantalon_noir_1.jpg,pantalon_noir_2.jpg','main' => 'pantalon_noir_1.jpg'),
+  array('id' => '3','name' => 'Sweat gris','size' => 'S','price' => '29.99','type' => 'sweat-shirt','gender' => 'enfant','gallery' => 'sweat_gris_1.jpg,sweat_gris_2.jpg,sweat_gris_3.jpg','main' => 'sweat_gris_1.jpg'),
+  array('id' => '4','name' => 'Pull rouge','size' => 'XL','price' => '49.99','type' => 'pull','gender' => 'unisexe','gallery' => 'pull_rouge_1.jpg,pull_rouge_2.jpg','main' => 'pull_rouge_1.jpg'),
+  array('id' => '5','name' => 'Chemise blanche','size' => 'M','price' => '34.99','type' => 'chemise','gender' => 'homme','gallery' => 'chemise_blanche_1.jpg,chemise_blanche_2.jpg,chemise_blanche_3.jpg','main' => 'chemise_blanche_1.jpg'),
+  array('id' => '6','name' => 'T-shirt vert','size' => 'S','price' => '18.99','type' => 't-shirt','gender' => 'femme','gallery' => 'tshirt_vert_1.jpg,tshirt_vert_2.jpg','main' => 'tshirt_vert_1.jpg'),
+  array('id' => '7','name' => 'Pantalon beige','size' => 'M','price' => '42.50','type' => 'pantalon','gender' => 'enfant','gallery' => 'pantalon_beige_1.jpg,pantalon_beige_3.jpg,pantalon_beige_2.jpg','main' => 'pantalon_beige_1.jpg'),
+  array('id' => '8','name' => 'Sweat bleu marine','size' => 'L','price' => '31.99','type' => 'sweat-shirt','gender' => 'unisexe','gallery' => 'sweat_marine_1.jpg,sweat_marine_2.jpg','main' => 'sweat_marine_1.jpg'),
+  array('id' => '9','name' => 'Pull noir','size' => 'M','price' => '45.00','type' => 'pull','gender' => 'homme','gallery' => 'pull_noir_1.jpg,pull_noir_2.jpg,pull_noir_3.jpg','main' => 'pull_noir_1.jpg'),
+  array('id' => '10','name' => 'Chemise à carreaux','size' => 'L','price' => '36.75','type' => 'chemise','gender' => 'femme','gallery' => 'chemise_carreaux_1.jpg,chemise_carreaux_2.jpg','main' => 'chemise_carreaux_1.jpg'),
+  array('id' => '11','name' => 'T-shirt imprimé','size' => 'XL','price' => '22.00','type' => 't-shirt','gender' => 'enfant','gallery' => 'tshirt_imprime_1.jpg,tshirt_imprime_2.jpg,tshirt_imprime_3.jpg','main' => 'tshirt_imprime_1.jpg'),
+  array('id' => '12','name' => 'Pantalon cargo','size' => 'S','price' => '40.00','type' => 'pantalon','gender' => 'unisexe','gallery' => 'pantalon_cargo_1.jpg,pantalon_cargo_2.jpg','main' => 'pantalon_cargo_1.jpg')
+);
