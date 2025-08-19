9bedcb965187397c2515a85bbb66cfd2c87a4e1d
daniel <danielaraux@hotmail.com>
2025-08-19 11:24:34 +0200

    en cours

--- src/accueil.php
+++ src/accueil.php
@@ -1,3 +1,8 @@
+<?php
+require_once('products/clothes.php');
+var_dump($_GET);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -6,11 +11,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Accueil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body class="text-center">
-    <h1>Page Accueil</h1>
+    <h1><a href="accueil.php">Page Accueil</a></h1>
 
+    <h2>Shop</h2>
+
+    <header>
+        <nav>
+            <div class="d-flex border justify-content-around p-5">
+                <a href="accueil.php?cat=t-shirt">T-shirts</a>
+                <a href="accueil.php?cat=pantalons">Pantalons</a>
+                <a href="accueil.php?cat=sweat-shirts">Sweat-shirts</a>
+                <a href="accueil.php?cat=pulls">Pulls</a>
+                <a href="accueil.php?cat=chemises">Chemises</a>
+            </div>
+        </nav>
+    </header>
+
+
+    <main>
+
+
+
+    </main>
 
 
 
